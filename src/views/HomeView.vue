bafaf58e46c93b3c969bbb7e69868ab3bcf23f84
Nero978 <zhumengfeiyang@qq.com>
2026-08-11 22:01:17 +0800

    feat: add HomeProblem component and update i18n translations for problem section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <HomeHero />
   <HomeData />
+  <HomeProblem />
   <FromNTU />
 </template>
 
 <script setup>
 import HomeHero from '@/components/HomeHero.vue'
 import HomeData from '@/components/HomeData.vue'
+import HomeProblem from '@/components/HomeProblem.vue'
 import FromNTU from '@/components/FromNTU.vue'
 </script>
