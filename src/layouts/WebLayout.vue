dfd0aad3086d85f51259e6b9325e5bbf28159e84
Nero978 <zhumengfeiyang@qq.com>
2026-08-12 23:46:19 +0800

    feat: add class to t-content for consistent styling in WebLayout

--- src/layouts/WebLayout.vue
+++ src/layouts/WebLayout.vue
@@ -2,7 +2,7 @@
   <t-layout>
     <NavBar />
 
-    <t-content>
+    <t-content class="main-content">
       <router-view />
     </t-content>
 
@@ -14,3 +14,9 @@
 import NavBar from '@/components/NavBar.vue'
 import PageFooter from '@/components/PageFooter.vue'
 </script>
+
+<style scoped>
+.main-content {
+  background-color: var(--td-bg-color-container);
+}
+</style>
