a1862f2f04f434d7ca9e44318b02bd568d010d42
Nero978 <zhumengfeiyang@qq.com>
2026-08-22 16:34:29 +0800

    feat(layout): restructure AppLayout component with NavBar and SiderBar integration

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,4 +1,42 @@
 <template>
-  applayout
-  <router-view />
+  <t-layout>
+    <NavBar class="nav-bar" />
+    <t-layout class="content-container">
+      <SiderBar />
+
+      <t-layout>
+        <t-content>
+          <RouterView />
+        </t-content>
+
+        <t-footer class="footer">
+          Copyright @ {{ new Date().getFullYear() }} NettedX. All Rights Reserved
+        </t-footer>
+      </t-layout>
+    </t-layout>
+  </t-layout>
 </template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import SiderBar from '@/components/SiderBar.vue'
+</script>
+
+<style scoped>
+.nav-bar {
+  position: fixed;
+}
+
+.content-container {
+  background-color: var(--td-bg-color-container);
+  min-height: 100svh;
+  padding-top: 3.5rem;
+}
+
+.footer {
+  text-align: center;
+  padding: 1rem;
+  font: var(--td-font-body-small);
+  color: var(--td-text-color-placeholder);
+}
+</style>
